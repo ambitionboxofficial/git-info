83d26d76110bc781638613c06a6438396149b87f
Abhishek Soni <abhisheksoni2720@gmail.com>
2020-03-22 20:17:12 +0530

    feat: better defaults for api urls

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,7 @@
-const dev = process.env.NODE_ENV === "development";
-export const API_URL = dev
-  ? process.env.REACT_APP_DEV_API
-  : process.env.REACT_APP_PROD_API;
+const { NODE_ENV, REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
+
+const dev = NODE_ENV === "development";
+const devAPI = REACT_APP_DEV_API ? REACT_APP_DEV_API : "";
+const prodAPI = REACT_APP_PROD_API ? REACT_APP_PROD_API : "";
+
+export const API_URL = dev ? devAPI : prodAPI;
