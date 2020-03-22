a9a954e3f063a096969d2936bcd91506292e7706
Abhishek Soni <abhisheksoni2720@gmail.com>
2020-03-22 18:36:46 +0530

    add main app loading state, move config to public

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,60 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Environment from "./components/Environment";
+import Loading from "./components/Loading";
 import "./App.css";
 
-function App({ config }) {
-  const { name, environments } = config;
-  const totalEnv = environments && environments.length;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
-  return (
-    <div className="App">
-      <Header appName={name}></Header>
-      <div
-        className="Env_Listing"
-        style={{
-          justifyContent: totalEnv < 2 ? "space-between" : "space-around"
-        }}
-      >
-        {environments.map((env, index) => {
-          return <Environment data={env} key={index}></Environment>;
-        })}
+  componentDidMount = () => {
+    fetch("/config.json")
+      .then(data => data.json())
+      .then(data => {
+        this.setState({ ...data, loading: false });
+      });
+  };
+
+  render() {
+    const { name, environments, loading } = this.state;
+    const totalEnv = environments && environments.length;
+
+    return (
+      <div className="App">
+        <Header appName={name}></Header>
+
+        {loading && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "70vh"
+            }}
+          >
+            <Loading height={50} width={50}></Loading>
+          </div>
+        )}
+
+        {!loading && (
+          <>
+            <div
+              className="Env_Listing"
+              style={{
+                justifyContent: totalEnv < 2 ? "space-between" : "space-around"
+              }}
+            >
+              {environments.map((env, index) => {
+                return <Environment data={env} key={index}></Environment>;
+              })}
+            </div>
+          </>
+        )}
+        <Footer></Footer>
       </div>
-      <Footer></Footer>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import config from "./config";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App config={config} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
